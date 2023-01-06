24bac23dc85bed22ec6095334a687b598d4f07d3
Ankit Rana <109352490+AnkitRana26@users.noreply.github.com>
2023-01-06 23:51:52 +0530

    Connection Build

--- server/server.js
+++ server/server.js
@@ -49,6 +49,130 @@ const io = require('socket.io')(server, {
     }
 })
 
+io.on('connection',(socket)=>{
+
+    console.log('User Connected ',socket.id);
+    socket.on('create-new-room',(data)=>{
+        createNewRoomHandler(data,socket);
+    })
+    socket.on('join-room',(data)=>{
+
+        joinRoomhandler(data,socket);
+
+    })
+    socket.on('disconnect',()=>{
+        disconnectHandler(socket);
+    })
+
+})
+
+
+// Socket.io Handlers
+
+
+const createNewRoomHandler =(data,socket)=>{
+    console.log(`host is creating new room` );
+    console.log(data);
+    const {identity} = data;
+
+    const roomId = uuidv4();
+
+    //create new user 
+    const newUser ={
+        identity,
+        id:uuidv4(),
+        socketId:socket.id,
+        roomId
+    }
+
+    //Push That user to Connected Users
+    connectedUsers = [...connectedUsers,newUser];
+
+    //create new room 
+
+    const newRoom = {
+        id:roomId,
+        connectedUsers:[newUser]
+    }
+
+    //Join Socket.io room
+    socket.join(roomId);
+
+    rooms =  [...rooms,newRoom];
+
+    //emit to that client which created that room roomId
+
+    socket.emit('room-id',{roomId});
+
+    //emit an event to all users connected to that room about new users which are right nin this room
+
+    socket.emit('room-update',{connectedUsers:newRoom.connectedUsers});
+
+
+
+
+}
+
+const joinRoomhandler =(data,socket)=>{
+
+    const {identity,roomId} = data;
+    const newUser ={
+        identity,
+        id:uuidv4(),
+        socketId:socket.id,
+        roomId
+    }
+
+    //Join Room as User which just is trying to join room passing room id
+    const room = rooms.find(room=>room.id==roomId);
+    room.connectedUsers = [...room.connectedUsers,newUser];
+
+
+    //Join socket.io Room
+    socket.join(roomId);
+
+    //add new User to Connected user array;
+    connectedUsers = [...connectedUsers,newUser];
+
+    io.to(roomId).emit('room-update',{connectedUsers:room.connectedUsers});
+
+
+
+
+}
+
+
+const disconnectHandler =(socket)=>{
+    const user = connectedUsers.find((user)=>user.socketId ===socket.id);
+    if(user){
+        //remove user from room  in server;
+        const room = rooms.find(room=>room.id===user.roomId);
+        room.connectedUsers = room.connectedUsers.filter(user=>user.socketId !==socket.id);
+
+        socket.leave(user.roomId);
+
+        
+
+        //Closing Room After All Users are getting exit;
+
+        if(room.connectedUsers.length > 0){
+            io.to(room.id).emit('room-update',{
+                connectedUsers:room.connectedUsers
+            });
+        }
+        else{
+            rooms = room.filter((r)=>r.id!=room.id);
+        }
+
+
+    }
+
+
+}
+
+
+
+
 server.listen(PORT, () => {
     console.log(`Server is Listening on ${PORT}`);
 })
