abf04744e1612c8c1f6884952a7d1147562dcc6a
Ankit Rana <109352490+AnkitRana26@users.noreply.github.com>
2023-01-22 14:02:41 +0530

    .

--- client/src/Pages/RoomPage/RoomLabel.js
+++ client/src/Pages/RoomPage/RoomLabel.js
@@ -1,13 +1,46 @@
 import { Box } from '@mui/material';
 import React from 'react'
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
+import { toast, ToastContainer } from 'react-toastify';
 
-const RoomLabel = ({roomId}) => {
-    
+const RoomLabel = ({ roomId }) => {
+
+
+
+  const copyToClipboard = (e) => {
+    navigator.clipboard.writeText(e.target.innerText);
+    toast.success('Copied To Clipboard', {
+      position: "top-right",
+      autoClose: 1500,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    })
+  }
+
   return (
-    <Box bgcolor='#182329' className='room_label'>
-        <p className='room_label_paragraph'>RoomId: {roomId}</p>
-    </Box>
+    <>
+      <Box bgcolor='#182329' className='room_label'>
+        <p className='room_label_paragraph'>RoomId: <span onClick={copyToClipboard} className='roomIdCopyToClipboard'>{roomId}</span></p>
+      </Box>
+      <ToastContainer
+        className="toaster"
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+      <ToastContainer />
+    </>
   )
 }
 
